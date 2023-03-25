964de053c728f6f734dd0c6e01536c29bf2932cf
Bascum Macik <bascummacik@gmail.com>
2023-03-25 16:28:36 -0500

    Added support for longer strings and order of operations

--- calculator.py
+++ calculator.py
@@ -1,18 +1,48 @@
 class Calculator:
     def __init__(self):
-        self.currentOp
+        self.wholeOperation = input("FORMAT: num (space) operator (space) num (space)...")
+        self.WholeOperationArr = []
+        self.operations = []
+        self.numbers = []
         self.history = []
         
-    def calc(self, num1, operation, num2):
-        if operation == "+":
-            self.add(num1, num2)
-        elif operation == "-":
-            self.sub(num1, num2)
-        elif operation == "*":
-            self.mult(num1, num2)
-        elif operation == "/":
-            self.div(num1, num2)
-            
+        self.formatOperation()
+        self.calc()
+        print(self.numbers)
+        
+    def formatOperation(self):
+        self.WholeOperationArr = self.wholeOperation.split()
+        for idx, i in enumerate(self.WholeOperationArr):
+            if idx % 2 == 0 or idx == 0:
+                self.numbers.append(int(i))
+            else:
+                self.operations.append(i)
+        
+    def calc(self, counter = 0):
+        for idx, i in enumerate(self.operations):
+            if i == "*" or i == "x" or i == "X" or i == "/":
+                if i == "*" or i == "x" or i == "X":
+                    self.numbers[idx] = self.mult(self.numbers[idx], self.numbers[idx + 1])
+                    del self.numbers[idx + 1]
+                    del self.operations[idx]
+                elif i == "/":
+                    self.numbers[idx] = self.div(self.numbers[idx], self.numbers[idx + 1])
+                    del self.numbers[idx + 1]
+                    del self.operations[idx]
+            elif (i == "+" or i == "-") and counter > 0:
+                if i == "+":
+                    self.numbers[idx] = self.add(self.numbers[idx], self.numbers[idx + 1])
+                    del self.numbers[idx + 1]
+                    del self.operations[idx]
+                elif i == "-":
+                    self.numbers[idx] = self.mult(self.numbers[idx], self.numbers[idx + 1])
+                    del self.numbers[idx + 1]
+                    del self.operations[idx]
+                    
+        if len(self.numbers) > 1:
+            counter += 1
+            self.calc(counter)
+
     def add(self, num1, num2):
         return num1 + num2
     
